194a88f4e7dc8948b4174b51497a2b578cc87e25
Leandro Batista <leandrobcrv@gmail.com>
2022-10-21 15:38:01 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <>
         <Header />
         <SolarSystem />
-      </div>
+        <Title />
+      </>
 
     );
   }
